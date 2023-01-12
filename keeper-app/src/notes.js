9ea367167547bb922d1e94edfd25dae742a4a84a
W. Yang-Lee Young <celalkincross@gmail.com>
2023-01-12 08:14:12 +0800

    add notes.js file

---
+++ keeper-app/src/notes.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const notes = [
+    {
+        key:1,
+        title: 'Arrays',
+        content: 'Testing the note function to see if it works'
+    }
+]
+
+
+export default notes;
