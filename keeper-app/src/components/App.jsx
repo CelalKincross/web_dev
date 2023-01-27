ab0df225356d96ac2fbe8b407f8521f8822569cb
W. Yang-Lee Young <celalkincross@gmail.com>
2023-01-27 10:55:57 +0800

    add user notes to UI

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,30 +2,32 @@ import React, { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
-import notes1 from "../notes"
 import CreateArea from "./CreateArea";
 // note section of app
 function App() {
-  const [notes, setNotes] = useState({})
+  const [notes, setNotes] = useState([])
 
-  function addNote(title, content) {
-    const newNote= {'title': title, 'content': content};
-    console.log(newNote)
-    // setNotes((prevNotes) => {
-    //   return ({...prevNotes, newNote})});
+  function addNote(title) {
+    setNotes((prevItems) => {
+      return ([
+        ...prevItems, title
+      ])
+    })
   }
 
     return (
     <div>
         <Header />
         <CreateArea addNote={addNote} />
-        {/* {notes.map(noteItem => (
-      <Note 
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-      />)
-      )} */}
+        {notes.map((note, index) => 
+          <Note 
+          key={index}
+          id={index}
+          title={note.title} 
+          content={note.content} 
+          />
+        )}
+        
         <Footer />
     </div>);
 }
--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,28 +1,33 @@
 import React, {useState} from 'react';
 
 function CreateArea(props) {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("")
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
 
-    function handleTitle(e) {
-        const value=e.target.value;
-        console.log(value)
-        setTitle(value);
+    function handleChanges(e) {
+        const { name, value }=e.target;
+        console.log(e.target)
+        setNote((prevNotes) => ({
+            ...prevNotes, [name]: value
+        }));
       }
-    
-    function handleContent(e) {
-        const value=e.target.value;
-        console.log(value)
-        setContent(value);
-    }
+
 
     return (
         <div>
-            <form>
-                <input name='title' onChange={handleTitle} placeholder='Title' />
-                <textarea name='content' onChange={handleContent} placeholder='Take notes...'/>
-                <button onClick={()=>
-                    props.addNote(title, content)}>Add</button>
+            <form onSubmit={(e)=>{
+                    props.addNote(note);
+                    setNote({
+                        title: "",
+                        content: ""
+                        })
+                    e.preventDefault();
+                    }}>
+                <input name='title' value={note.title} onChange={handleChanges} placeholder='Title' />
+                <textarea name='content' value={note.content} onChange={handleChanges} placeholder='Take notes...'/>
+                <button type="submit">Add</button>
             </form>
         </div>
     )
