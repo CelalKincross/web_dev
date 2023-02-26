d67d9b28fc616e65043519c4bc8c04676fac1b0f
W. Yang-Lee Young <celalkincross@gmail.com>
2023-02-26 15:10:50 +0800

    finish express practice and update readme

---
+++ bmi-express/bmi.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser")
+const url = bodyParser.urlencoded({extended:false})
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", url, (req, res)=> {
+    const weight = Number(req.body.weight);
+    const height = Number(req.body.height);
+    
+    const bmi = weight / ((height/100)**2);
+    res.send(`Your BMI is: ${bmi.toFixed(2)}`)
+    console.log(req.body.weight)
+    console.log(req.body.height)
+})
+
+app.get("/calculator", (req, res) => {
+    res.sendFile(__dirname + "/calculator.html")
+})
+
+app.post("/calculator", url, (req, res) => {
+    const num1 = Number(req.body.n1);
+    const num2 = Number(req.body.n2);
+    const result = num1 + num2;
+    res.send(`${num1} plus ${num2} is ${result}`);
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is running on port 3000!")
+})
